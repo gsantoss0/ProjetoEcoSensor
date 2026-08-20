06bfa2d995b213f66532f1a77c512cd78bc93f7f
Arthur Lopes Amorim Faleiro <arthurvc132@gmail.com>
2026-08-20 11:37:32 -0300

    Versao com ventoinha arduino

--- Firmware/Arduino-CODE.ino
+++ Firmware/Arduino-CODE.ino
@@ -2,51 +2,80 @@
 
 #define DHTPIN 2    
 #define DHTTYPE DHT11 
+#define rele 8
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(rele, OUTPUT);
 
   Serial.begin(9600);
 
-  dht.begin(); 
+  dht.begin();
 }
 
+ //------------------------------------------------
+
 void loop() {
-  delay(2000); 
+  delay(2000);
 
-  float h = dht.readHumidity();    
-  float t = dht.readTemperature(); 
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
+ //------------------------------------------------
 
   if (isnan(h) || isnan(t)) {
     Serial.println("Erro ao ler do sensor DHT!");
     return;
   }
 
-  Serial.print("Umidade: ");
-  Serial.print(h);
-  Serial.print("%  ");
-  Serial.print("Temperatura: ");
+  //------------------------------------------------
+
   Serial.print(t);
-  Serial.println("°C");
+  Serial.print(",");
+  Serial.println(h);
 
-  if (t >= 28)
-  {
-  Serial.println("ALERTA... Alta temperatura!");
-  digitalWrite(11, HIGH);
+ //------------------------------------------------
+ //LED vermelho (alta temperatura)
+  if (t >= 28){
+  digitalWrite(rele, LOW);
+  digitalWrite(10, HIGH);
+  tone(3, 500);
+
   delay(1000);
-  digitalWrite(11, LOW);
+
+  digitalWrite(rele, LOW);
+  digitalWrite(10, LOW);
+  noTone(3);
+
   delay(1000);
   }
 
-  if (h <= 30 || h >= 60)
-  {
-  Serial.println("ALERTA... Alto ou baixo nivel de humidade!");
+//LED amarelo (alto nivel de umidade)
+  else if (h >= 60){
+
+  digitalWrite(rele, LOW);
   digitalWrite(11, HIGH);
+  tone(3, 500);
+
   delay(1000);
+
   digitalWrite(11, LOW);
+  noTone(3);
+
   delay(1000);
+
   }
+  //LED verde (se estiver tudo certo)
+  else {
+    digitalWrite(9, HIGH);
+    delay(1000);
+    digitalWrite(9, LOW);
+    digitalWrite(rele, HIGH);
+  }
+
 }
